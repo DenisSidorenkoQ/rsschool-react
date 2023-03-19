7b03b1036cb03d1e3852139c64f0a6cc6af1cc56
Denis <apricot103@mail.ru>
2023-03-20 01:02:34 +0300

    feat: added header on error page

--- src/pages/Error404.tsx
+++ src/pages/Error404.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Header } from './Header';
 
 export const Error404 = () => {
   return (
     <div>
+      <Header />
       <h1>Error 404</h1>
     </div>
   );
