da0c7f82c8ddc8b7977d071c7100ff0562af61b9
Denis <apricot103@mail.ru>
2023-04-01 23:38:14 +0300

    refactor: rewritten main to hooks

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,21 +6,19 @@ type Props = {
   job: string;
 };
 
-export class Card extends React.Component<Props> {
-  render() {
-    const { img, name, job } = this.props;
-    return (
-      <>
-        <div className="card">
-          <img src={img} alt="Avatar" />
-          <div className="container">
-            <h4>
-              <b>{name}</b>
-            </h4>
-            <p>{job}</p>
-          </div>
+export function Card(props: Props) {
+  const { img, name, job } = props;
+  return (
+    <>
+      <div className="card">
+        <img src={img} alt="Avatar" />
+        <div className="container">
+          <h4>
+            <b>{name}</b>
+          </h4>
+          <p>{job}</p>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,32 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './SearchBar.css';
 
 const LOCAL_STORAGE_VALUE_NAME = 'SAVED_VALUE';
 
-export class SearchBar extends React.Component {
-  state = {
-    inputText: localStorage.getItem(LOCAL_STORAGE_VALUE_NAME) || '',
+export function SearchBar() {
+  const [inputText, setInputText] = React.useState(
+    localStorage.getItem(LOCAL_STORAGE_VALUE_NAME) || ''
+  );
+
+  useEffect(() => {
+    return function () {
+      localStorage.setItem(LOCAL_STORAGE_VALUE_NAME, inputText);
+    };
+  }, [inputText]);
+
+  const handleChangeText = (event: { target: { value: string } }) => {
+    setInputText(event.target.value);
+    console.log(inputText);
   };
 
-  componentWillUnmount() {
-    localStorage.setItem(LOCAL_STORAGE_VALUE_NAME, this.state.inputText);
-  }
-
-  handleChangeText = (event: { target: { value: string } }) => {
-    this.setState({ inputText: event.target.value });
-  };
-
-  render() {
-    return (
-      <div className="box">
-        <input
-          className="search-bar"
-          type="text"
-          placeholder="Input text"
-          value={this.state.inputText}
-          onChange={this.handleChangeText}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className="box">
+      <input
+        className="search-bar"
+        type="text"
+        placeholder="Input text"
+        value={inputText}
+        onChange={handleChangeText}
+      />
+    </div>
+  );
 }
