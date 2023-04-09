79f2e1c0bb365958b641e6d6e2d1d0c939277fc3
Denis <apricot103@mail.ru>
2023-04-09 14:52:25 +0300

    feat: add close modal window when click outside

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -7,9 +7,9 @@ interface Props {
 
 export const Modal = (props: Props) => {
   return (
-    <div className="overlay">
-      <div className="modalBackground" onClick={() => props.modalIsOpen(false)}>
-        <div className="modalContainer">
+    <div className="overlay" onClick={(e) => props.modalIsOpen(false)}>
+      <div className="modalBackground">
+        <div className="modalContainer" onClick={(e) => e.stopPropagation()}>
           <div className="titleCloseBtn">
             <button onClick={() => props.modalIsOpen(false)}> X </button>
           </div>
